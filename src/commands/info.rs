64dcb8ffec4275b2c6f1f4f9c8fdc9f5a04e26f4
icrayix <icrayix@gmail.com>
2024-05-29 12:12:35 +0200

    fix: only preserve 3 most significant units of uptime duration

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -10,18 +10,25 @@ use crate::Ctx;
 pub async fn info(ctx: Ctx<'_>) -> Result<(), crate::Error> {
     let user = ctx.framework().bot_id.to_user(&ctx.http()).await?;
     let ping = ctx.ping().await;
+    let ping = ((ping.as_secs_f32() * 1000.0).round() / 1000.0).to_string() + "s";
+    let uptime = humantime::format_duration(Instant::now() - ctx.data().startup_time)
+        .to_string()
+        .split(' ')
+        .take(3) // only preserve the 3 most significant units in the formatted duration
+        .collect::<Vec<_>>()
+        .join(" ");
 
     let embed = CreateEmbed::default()
             .title(user.name.to_string())
             .description(format!(
-            r#"
-                >>> **Source**: {}
-                **Uptime**: `{}`
-                **API Ping**: `{}`
-            "#,
-            "[neuon-from-ploudos/ploudos-mod-bot](https://github.com/neuon-from-ploudos/ploudos-mod-bot)",
-            humantime::format_duration(Instant::now() - ctx.data().startup_time),
-            ((ping.as_secs_f32() * 1000.0).round() / 1000.0).to_string() + "s")
+                r#"
+                    >>> **Source**: {}
+                    **Uptime**: `{}`
+                    **API Ping**: `{}`
+                "#,
+                "[neuon-from-ploudos/ploudos-mod-bot](https://github.com/neuon-from-ploudos/ploudos-mod-bot)",
+                uptime,
+                ping)
             )
             .footer(CreateEmbedFooter::new(format!("Version: {}", env!("CARGO_PKG_VERSION"))));
 
